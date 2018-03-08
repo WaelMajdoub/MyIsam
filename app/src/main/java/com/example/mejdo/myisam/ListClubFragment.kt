7b45ec9411068ce78c92319d00bf4ab904fc361e
syrineKarwi <karwi.syrine@gmail.com>
2018-03-08 23:34:32 +0100

    design

--- app/src/main/java/com/example/mejdo/myisam/ListClubFragment.kt
+++ app/src/main/java/com/example/mejdo/myisam/ListClubFragment.kt
@@ -1,22 +1,29 @@
 package com.example.mejdo.myisam
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 
+@Suppress("UNREACHABLE_CODE")
 /**
  * A simple [Fragment] subclass.
  */
 class ListClubFragment : Fragment() {
 
+    lateinit var text : TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
         return inflater!!.inflate(R.layout.fragment_list_club, container, false)
+
+
     }
 
     companion object {
