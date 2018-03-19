84086c960b3b65a09691b1b3b158c1e1e89ff543
syrineKarwi <karwi.syrine@gmail.com>
2018-03-19 21:27:43 +0100

    add event

---
+++ app/src/main/java/com/example/mejdo/myisam/fragments/add/AddEventFragment.kt
@@ -0,0 +1,106 @@
+package com.example.mejdo.myisam.fragments.add
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+import com.example.mejdo.myisam.R
+import com.example.mejdo.myisam.model.Clubs
+import com.example.mejdo.myisam.model.Events
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AddEventFragment : Fragment() {
+
+    lateinit var save: Button
+    lateinit var editText1 : EditText
+    lateinit var editText2 : EditText
+    lateinit var editText3 : EditText
+    lateinit var editText4 : EditText
+
+    lateinit var mAuth: FirebaseAuth
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_add_event, container, false)
+
+        save = view.findViewById<Button>(R.id.save)
+        editText1 = view.findViewById<EditText>(R.id.name)
+        editText2 = view.findViewById<EditText>(R.id.type)
+        editText3 = view.findViewById<EditText>(R.id.description)
+        editText4 = view.findViewById<EditText>(R.id.date)
+
+        save.setOnClickListener{
+            val name= editText1.text.toString().trim()
+            val type= editText2.text.toString().trim()
+            val description= editText3.text.toString().trim()
+            val date= editText4.text.toString().trim()
+
+            mAuth=FirebaseAuth.getInstance()
+            val uid=mAuth.currentUser?.uid
+
+
+           //    val ref =FirebaseDatabase.getInstance().getReference("Clubs")
+
+            val myDataBase= FirebaseDatabase.getInstance().getReference("Events")
+            val eventId = myDataBase.push().key
+            val ref= FirebaseDatabase.getInstance().getReference("Clubs")
+          /*  val query = ref.orderByChild("iduser").equalTo(uid)
+
+            query.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                    for (Clubs in snapshot.children) {
+                        val id= Clubs.child("clubId").ref
+                        Toast.makeText(view.context,"Saved succesfuly."+id,Toast.LENGTH_SHORT).show()
+                         val event = Events(eventId, name, type, description ,date,id)
+
+                           myDataBase.child(eventId).setValue(event).addOnCompleteListener{
+                               Toast.makeText(view.context,"Saved succesfuly.", Toast.LENGTH_SHORT).show()
+                           }
+
+
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Log.w(TAG, "getUser:onCancelled", databaseError.toException())
+                }
+            })*/
+
+
+
+
+        }
+        return view
+
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance(): AddEventFragment = AddEventFragment()
+    }
+
+    fun saveData(){
+
+
+    }
+}// Required empty public constructor
